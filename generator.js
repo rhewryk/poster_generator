2939b5ce4daf5086d959e746bd72dbb229265a42
rhewryk <78235971+rhewryk@users.noreply.github.com>
2021-12-08 22:25:50 -0500

    sc

--- generator.js
+++ generator.js
@@ -1,98 +1,233 @@
-const objects = [];
-let eyeZ;
+
+
+const Y_AXIS = 1;
+const X_AXIS = 2;
+let b1, b2, c1, c2;
 
 function setup() {
-  createCanvas(710, 400, WEBGL);
+  createCanvas(2400, 3600);
 
-  eyeZ = height / 2 / tan((30 * PI) / 180); // The default distance the camera is away from the origin.
+ 
 
-  objects.push(new IntersectPlane(1, 0, 0, -100, 0, 0)); // Left wall
-  objects.push(new IntersectPlane(1, 0, 0, 100, 0, 0)); // Right wall
-  objects.push(new IntersectPlane(0, 1, 0, 0, -100, 0)); // Bottom wall
-  objects.push(new IntersectPlane(0, 1, 0, 0, 100, 0)); // Top wall
-  objects.push(new IntersectPlane(0, 0, 1, 0, 0, 0)); // Back wall
 
-  noStroke();
-  ambientMaterial(250);
+
+
+  // define colors
+  // b1 = color(255);
+  // b2 = color(0);
+  // c1 = color(204, 102, 0);
+  // c2 = color(0, 102, 153);
+
+//primary colors
+  p1 = color(237, 28, 36) //red
+  p2 = color(255, 242, 0) //yellow
+  p3 = color(46, 49, 146) //blue
+
+//complementary colors
+c1 = p1 //red
+c2 = color(0, 166, 81) //green
+c3 = p2 //yellow
+c4 = color(127, 63, 152) //purple
+c5 = color(0, 128, 255) //blue(lighter)
+c6 = color(242, 101, 34) //orange
+
+//analogous colors
+// a1 = 
+
+
+
+  noLoop();
 }
+
+
+
+
 
 function draw() {
-  background(0);
+  // // background 
 
-  // Lights
-  pointLight(255, 255, 255, 0, 0, 400);
-  ambientLight(244, 122, 158);
 
-  // Left wall
-  push();
-  translate(-100, 0, 200);
-  rotateY((90 * PI) / 180);
-  plane(400, 200);
-  pop();
+                  //PRIMARY COLOR COMBINATIONS:
+                     setGradient(0, 0, width / 2, height, c5, c6, X_AXIS);
+                     // setGradient(0, 0, width / 2, height, p2, p3, X_AXIS);
+                     // setGradient(0, 0, width / 2, height, p3, p1, X_AXIS);
 
-  // Right wall
-  push();
-  translate(100, 0, 200);
-  rotateY((90 * PI) / 180);
-  plane(400, 200);
-  pop();
 
-  // Bottom wall
-  push();
-  translate(0, 100, 200);
-  rotateX((90 * PI) / 180);
-  plane(200, 400);
-  pop();
 
-  // Top wall
-  push();
-  translate(0, -100, 200);
-  rotateX((90 * PI) / 180);
-  plane(200, 400);
-  pop();
 
-  plane(200, 200); // Back wall
 
-  const x = mouseX - width / 2;
-  const y = mouseY - height / 2;
 
-  const Q = createVector(0, 0, eyeZ); // A point on the ray and the default position of the camera.
-  const v = createVector(x, y, -eyeZ); // The direction vector of the ray.
 
-  let intersect; // The point of intersection between the ray and a plane.
-  let closestLambda = eyeZ * 10; // The draw distance.
 
-  for (let x = 0; x < objects.length; x += 1) {
-    let object = objects[x];
-    let lambda = object.getLambda(Q, v); // The value of lambda where the ray intersects the object
+  // setGradient(width / 2, 0, width / 2, height, c2, c1, X_AXIS);
+  // foreground
+  // setGradient(50, 90, 540, 80, c1, c2, Y_AXIS);
+  // setGradient(50, 190, 540, 80, c2, c1, X_AXIS); 
+}
 
-    if (lambda < closestLambda && lambda > 0) {
-      // Find the position of the intersection of the ray and the object.
-      intersect = p5.Vector.add(Q, p5.Vector.mult(v, lambda));
-      closestLambda = lambda;
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
+
+
+  if (axis === X_AXIS) {
+    // left to right gradient
+    for (let i = x; i <= x + w; i++) {
+      let inter = map(i, x, x + w, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(i, y, i, y +h);
     }
   }
-
-  // Cursor
-  push();
-  translate(intersect);
-  fill(237, 34, 93);
-  sphere(10);
-  pop();
 }
 
-// Class for a plane that extends to infinity.
-class IntersectPlane {
-  constructor(n1, n2, n3, p1, p2, p3) {
-    this.normal = createVector(n1, n2, n3); // The normal vector of the plane
-    this.point = createVector(p1, p2, p3); // A point on the plane
-    this.d = this.point.dot(this.normal);
-  }
+$("#keep").click(function(){
+  saveCanvas("poster","jpg")
+})
 
-  getLambda(Q, v) {
-    return (-this.d - this.normal.dot(Q)) / this.normal.dot(v);
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const objects = [];
+// let eyeZ;
+
+// function setup() {
+//   createCanvas(710, 400, WEBGL);
+
+//   eyeZ = height / 2 / tan((30 * PI) / 180); // The default distance the camera is away from the origin.
+
+//   objects.push(new IntersectPlane(1, 0, 0, -100, 0, 0)); // Left wall
+//   objects.push(new IntersectPlane(1, 0, 0, 100, 0, 0)); // Right wall
+//   objects.push(new IntersectPlane(0, 1, 0, 0, -100, 0)); // Bottom wall
+//   objects.push(new IntersectPlane(0, 1, 0, 0, 100, 0)); // Top wall
+//   objects.push(new IntersectPlane(0, 0, 1, 0, 0, 0)); // Back wall
+
+//   noStroke();
+//   ambientMaterial(250);
+// }
+
+// function draw() {
+//   background(0);
+
+//   // Lights
+//   pointLight(255, 255, 255, 0, 0, 400);
+//   ambientLight(244, 122, 158);
+
+//   // Left wall
+//   push();
+//   translate(-100, 0, 200);
+//   rotateY((90 * PI) / 180);
+//   plane(400, 200);
+//   pop();
+
+//   // Right wall
+//   push();
+//   translate(100, 0, 200);
+//   rotateY((90 * PI) / 180);
+//   plane(400, 200);
+//   pop();
+
+//   // Bottom wall
+//   push();
+//   translate(0, 100, 200);
+//   rotateX((90 * PI) / 180);
+//   plane(200, 400);
+//   pop();
+
+//   // Top wall
+//   push();
+//   translate(0, -100, 200);
+//   rotateX((90 * PI) / 180);
+//   plane(200, 400);
+//   pop();
+
+//   plane(200, 200); // Back wall
+
+//   const x = mouseX - width / 2;
+//   const y = mouseY - height / 2;
+
+//   const Q = createVector(0, 0, eyeZ); // A point on the ray and the default position of the camera.
+//   const v = createVector(x, y, -eyeZ); // The direction vector of the ray.
+
+//   let intersect; // The point of intersection between the ray and a plane.
+//   let closestLambda = eyeZ * 10; // The draw distance.
+
+//   for (let x = 0; x < objects.length; x += 1) {
+//     let object = objects[x];
+//     let lambda = object.getLambda(Q, v); // The value of lambda where the ray intersects the object
+
+//     if (lambda < closestLambda && lambda > 0) {
+//       // Find the position of the intersection of the ray and the object.
+//       intersect = p5.Vector.add(Q, p5.Vector.mult(v, lambda));
+//       closestLambda = lambda;
+//     }
+//   }
+
+//   // Cursor
+//   push();
+//   translate(intersect);
+//   fill(237, 34, 93);
+//   sphere(10);
+//   pop();
+// }
+
+// // Class for a plane that extends to infinity.
+// class IntersectPlane {
+//   constructor(n1, n2, n3, p1, p2, p3) {
+//     this.normal = createVector(n1, n2, n3); // The normal vector of the plane
+//     this.point = createVector(p1, p2, p3); // A point on the plane
+//     this.d = this.point.dot(this.normal);
+//   }
+
+//   getLambda(Q, v) {
+//     return (-this.d - this.normal.dot(Q)) / this.normal.dot(v);
+//   }
+// }
 
 
 
