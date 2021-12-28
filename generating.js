74fe1171d678a7e7e63532392d4257c91b9d8c29
rhewryk <78235971+rhewryk@users.noreply.github.com>
2021-12-28 11:09:35 -0500

    7

---
+++ generating.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+function setup() {
+  // createCanvas(200, 300);
+  // textSize(120);
+  // textAlign(CENTER);
+
+
+
+    createCanvas(2400, 1500);
+
+    createGraphics(width, height);
